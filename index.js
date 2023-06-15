7477058db57fe4d0256f066d9a3c89a5fe0abbd0
lmurr248 <55932123+lmurr248@users.noreply.github.com>
2023-06-15 06:39:37 +0000

    Created the index page and set up the form.

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ function helloWorld(){
 var helloWorld = "Hello World!";
 document.getElementById("header").innerHTML = helloWorld;
 }
+
+
+// Tiny MCE
+
+tinymce.init({
+      selector: '#text-editor',
+      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    });
+
+// Copy button text to clipboard
+
+const copyButton = document.getElementById('copyButton');
+
+      copyButton.addEventListener('click', () => {
+        const buttonContent = copyButton.textContent;
+
+        navigator.clipboard.writeText(buttonContent)
+          .then(() => {
+            console.log('Button content copied to clipboard: ' + buttonContent);
+          })
+          .catch(err => {
+            console.error('Failed to copy button content: ', err);
+            alert('Failed to copy button content. Please try again.');
+          });
+      });
